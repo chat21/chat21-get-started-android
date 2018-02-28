750b9c68da53d05012dca4dafd2be519f574dba2
Stefano De Pascalis <stefanodp91@gmail.com>
2018-02-28 10:09:26 +0100

    signin with email and password

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# v.: 1.0.1 b.: 2
+- signin with email and password
--- app/src/main/java/it/frontiere21/mychat/MainActivity.java
+++ app/src/main/java/it/frontiere21/mychat/MainActivity.java
@@ -7,6 +7,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.chat21.android.core.ChatManager;
 import org.chat21.android.core.authentication.ChatAuthentication;
+import org.chat21.android.core.contacts.listeners.OnContactCreatedCallback;
+import org.chat21.android.core.exception.ChatRuntimeException;
 import org.chat21.android.core.users.models.IChatUser;
 import org.chat21.android.ui.ChatUI;
 
@@ -19,17 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-        ChatManager.startAnonymously(this, getString(R.string.chat_firebase_appId),
-                        new ChatAuthentication.OnChatLoginCallback() {
-                            @Override
-                            public void onChatLoginSuccess(IChatUser currentUser) {
-                                ChatUI.getInstance().openConversationsListActivity();
-                            }
+        ChatManager.startWithEmailAndPassword(this, getString(R.string.google_app_id),
+                "john@nashville.it", "123456", new ChatAuthentication.OnChatLoginCallback() {
+                    @Override
+                    public void onChatLoginSuccess(IChatUser currentUser) {
+                        ChatManager.getInstance().createContactFor(currentUser.getId(), currentUser.getEmail(),
+                                "john", "Nashville", new OnContactCreatedCallback() {
+                                    @Override
+                                    public void onContactCreatedSuccess(ChatRuntimeException exception) {
+                                        if (exception == null) {
+                                            ChatUI.getInstance().openConversationsListActivity();
+                                        } else {
+                                            // TODO: handle the exception
+                                        }
+                                    }
+                                });
+                    }
 
-                            @Override
-                            public void onChatLoginError(Exception e) {
-                                // TODO: 22/02/18
-                            }
-                        });
+                    @Override
+                    public void onChatLoginError(Exception e) {
+                        // TODO: 22/02/18
+                    }
+                });
     }
 }
